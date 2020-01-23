56426e212af83302ceaeb77cf05fb39430a4e1e4
12hleb <56984619+12hleb@users.noreply.github.com>
2020-01-22 22:11:20 -0600

    Update FirstTestCase.java

--- src/FirstTestCase.java
+++ src/FirstTestCase.java
@@ -3,9 +3,6 @@ public class FirstTestCase {
     public static void main(String[] args) {
         System.out.println("My first try");
         System.out.println("My second try");
-        int a = 7;
-        int b = 8;
-        System.out.println(a+b);
-        System.out.println(a+b);
+        
     }
 }
