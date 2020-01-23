186f240dfd12d0b9cfb92fac6e6e81e30978108c
admin <12hleb@gmail.com>
2020-01-22 21:45:50 -0600

    Integer added and printOut sum

--- src/FirstTestCase.java
+++ src/FirstTestCase.java
@@ -3,5 +3,8 @@ public class FirstTestCase {
     public static void main(String[] args) {
         System.out.println("My first try");
         System.out.println("My second try");
+        int a = 7;
+        int b = 8;
+        System.out.println(a+b);
     }
 }
